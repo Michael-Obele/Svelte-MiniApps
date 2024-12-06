acb678ed30c6d275f60405dd34a31bbaeeeeeb53
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-12-06 21:59:56 +0100

    🔧 chore(db): remove unused database configuration and schema
    - delete commented-out code related to environment variables
    - remove unused schema definition file to clean up the project

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,3 @@
-// import { dev } from '$app/environment';
-// import { env } from '$env/dynamic/private';
-// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-// if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
-
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
