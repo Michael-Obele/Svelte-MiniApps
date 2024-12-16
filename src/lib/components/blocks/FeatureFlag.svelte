b0c1855af9e2849c9623f5ced28575e2ad04d897
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-12-16 19:28:40 +0100

    ✨ feat ✨(flags): Introduce feature flag for conditional rendering - Enable dynamic content display based on feature status.
    - Added a feature flag to control content visibility.
    - Uncommented direct URL configuration in the environment example file.
    - Created a new Svelte component to render content conditionally.

---
+++ src/lib/components/blocks/FeatureFlag.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    interface Props {
+        children?: import('svelte').Snippet;
+    }
+
+    let { children }: Props = $props();
+    if (import.meta.env.VITE_FEATURE_FLAG) {
+        console.log('Feature flag is enabled');
+    }
+    </script>
+
+{#if import.meta.env.VITE_FEATURE_FLAG}
+{@render children?.()}
+{/if}
