feeb8f6802a7f68b5024dd664a193b6d6d368b99
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-01-01 15:14:07 +0100

    ✨ feat ✨(ui): Add search shortcut icon - Enhance UX with keyboard focus
    - Introduce keyboard shortcut for search input focus
    - Display icon to visually indicate shortcut availability
    - Improve user experience by streamlining search access

--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import List from './List.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
-
 	import { filter } from '$lib/utils';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import BlurInText from '@/components/blocks/BlurInText.svelte';
+	import { Command } from 'lucide-svelte'; // Import the Command icon from lucide-svelte
 
 	function setFilterValue(filterValue: string): () => void {
 		return () => {
@@ -13,9 +13,23 @@
 	}
 
 	let app = $state('');
+
+	// Function to focus the input when Ctrl+K is pressed
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.ctrlKey && event.key === 'k') {
+			event.preventDefault();
+			document.getElementById('app-search')?.focus();
+		}
+	}
+
+	// Add event listener for keydown
+	$effect(() => {
+		window.addEventListener('keydown', handleKeydown);
+		return () => {
+			window.removeEventListener('keydown', handleKeydown);
+		};
+	});
 </script>
-
-
 
 <BlurInText>
 	<h1 class="my-8 text-center text-4xl font-bold">Explore Our Mini Applications</h1>
@@ -32,13 +46,19 @@
 	>
 		All
 	</Button>
-	<Input
-		type="text"
-		name="app"
-		bind:value={app}
-		placeholder="Find App..."
-		class="order-last mx-5 mb-2 w-fit rounded-md border border-blue-300 px-3 py-2 placeholder-blue-500 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm md:order-none"
-	/>
+	<div class="relative order-last mx-5 mb-2 w-fit md:order-none">
+		<Input
+			id="app-search"
+			type="text"
+			name="app"
+			bind:value={app}
+			placeholder="Find App..."
+			class="rounded-md border border-blue-300 px-3 py-2 placeholder-blue-500 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+		/>
+		<div class="absolute inset-y-0 right-0 flex items-center pr-3 text-sm font-bold text-gray-400">
+			<Command class="size-4 text-gray-400" aria-roledescription="Press Ctrl+K to search" /> + K
+		</div>
+	</div>
 	<Button
 		onclick={setFilterValue('done')}
 		class={`mb-3 me-3 rounded-md px-5 py-2.5 text-center text-base font-medium focus:outline-none focus:ring-4 ${
