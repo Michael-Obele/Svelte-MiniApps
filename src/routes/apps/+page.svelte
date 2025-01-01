b0c0e3fb44a84e93930cb2928d63cc77b13cfffa
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-01-01 16:13:28 +0100

    ✨ chore 📦(deps): Update dependencies 📦 and UI enhancements
    - Updated bun.lockb to the latest version 📦
    - Upgraded development dependencies 📦 in package 📦.json 📈
    - Integrated new command shortcut component for improved UX ⌨️
    - Refined button styling logic for a more cohesive design 🎨 🎨

--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -4,7 +4,8 @@
 	import { filter } from '$lib/utils';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import BlurInText from '@/components/blocks/BlurInText.svelte';
-	import { Command } from 'lucide-svelte'; // Import the Command icon from lucide-svelte
+	import { Command } from 'lucide-svelte';
+	import * as Cmd from '$lib/components/ui/command/index.js';
 
 	function setFilterValue(filterValue: string): () => void {
 		return () => {
@@ -56,16 +57,20 @@
 			class="rounded-md border border-blue-300 px-3 py-2 placeholder-blue-500 focus:border-green-500 focus:outline-none focus:ring-green-500 focus-visible:ring-green-600 sm:text-sm"
 		/>
 		<div class="absolute inset-y-0 right-0 flex items-center pr-3 text-sm font-bold text-gray-400">
-			<Command class="size-4 text-gray-400" aria-roledescription="Press Ctrl+K to search" /> + K
+			<Cmd.Shortcut
+				class="flex items-center justify-center sm:text-base"
+				aria-roledescription="Press Ctrl+K to search"
+				><Command class="size-3 text-gray-400 sm:size-4" />+k</Cmd.Shortcut
+			>
+			<!-- <Command class="size-4 text-gray-400" aria-roledescription="Press Ctrl+K to search" /> + K -->
 		</div>
 	</div>
 	<Button
 		onclick={setFilterValue('done')}
-		class={`mb-3 me-3 rounded-md px-5 py-2.5 text-center text-base font-medium focus:outline-none focus:ring-4 ${
-			$filter === 'done'
-				? 'border border-white bg-green-300 text-green-900 hover:border-green-200 hover:bg-green-700 focus:ring-green-300 dark:border-green-900 dark:bg-green-900 dark:text-white dark:hover:border-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800'
-				: 'border border-green-600 bg-white text-green-700 hover:bg-green-700 hover:text-white focus:ring-green-300 dark:border-green-500 dark:bg-gray-900 dark:text-green-500 dark:hover:bg-green-500 dark:hover:text-white dark:focus:ring-green-800'
-		}`}
+		class="mb-3 me-3 rounded-md px-5 py-2.5 text-center text-base font-medium focus:outline-none focus:ring-4 {$filter ===
+		'done'
+			? 'border border-white bg-green-300 text-green-900 hover:border-green-200 hover:bg-green-700 focus:ring-green-300 dark:border-green-900 dark:bg-green-900 dark:text-white dark:hover:border-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800'
+			: 'border border-green-600 bg-white text-green-700 hover:bg-green-700 hover:text-white focus:ring-green-300 dark:border-green-500 dark:bg-gray-900 dark:text-green-500 dark:hover:bg-green-500 dark:hover:text-white dark:focus:ring-green-800'}"
 	>
 		Done
 	</Button>
