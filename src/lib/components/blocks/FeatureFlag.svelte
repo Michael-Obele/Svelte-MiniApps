e58e645ceb2077f722b6fb627f5ef3ee398dd16b
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-01-09 23:18:59 +0100

    ✨ feat ✨(components/FeatureFlag): Enhance feature flag logic to include privileged user roles

--- src/lib/components/blocks/FeatureFlag.svelte
+++ src/lib/components/blocks/FeatureFlag.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { cn } from '@/utils/cn';
 	import { PUBLIC_FEATURE_FLAG } from '$env/static/public';
+	import { userContext } from '@/utils';
 
 	interface Props {
 		children?: import('svelte').Snippet;
@@ -12,9 +13,15 @@
 
 	// Convert the string value to a boolean
 	const isFeatureFlagEnabled: boolean = JSON.parse(PUBLIC_FEATURE_FLAG || 'false');
+
+	// Check if the user is an admin or tester
+	const isPrivilegedUser: boolean = ['admin', 'tester'].includes($userContext?.role ?? '');
+
+	// Combine the conditions
+	const shouldRender: boolean = isFeatureFlagEnabled || isPrivilegedUser;
 </script>
 
-{#if isFeatureFlagEnabled}
+{#if shouldRender}
 	<div class={cn('container mx-auto my-8 px-4 py-8', className)}>
 		{@render children?.()}
 	</div>
