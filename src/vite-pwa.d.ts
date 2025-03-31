06785019cc8815aa159ab5079075ee013d4332ee
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-03-31 01:17:49 +0100

    🔧 chore 📦(deps): Update dependencies 📦 and improve TypeScript types - Refresh lockfile and enhance PWA type declarations.
    - Update bun.lockb to reflect changes in dependencies 📦.
    - Upgrade @vite-pwa/sveltekit version in package 📦.json.
    - Refactor TypeScript definitions for better readability and consistency.

--- src/vite-pwa.d.ts
+++ src/vite-pwa.d.ts
@@ -1,38 +1,44 @@
+/// <reference types="@vite-pwa/sveltekit" />
 /// <reference types="vite/client" />
 
 // Define the pwa info module
 declare module 'virtual:pwa-info' {
-  export interface PWAInfo {
-    pwaInDevEnvironment: boolean;
-    webManifest: {
-      href: string;
-      useCredentials: boolean;
-      linkTag: string;
-    };
-    registerSW?: (options?: RegisterSWOptions) => Promise<RegisterSWResult>;
-  }
-  export const pwaInfo: PWAInfo | undefined;
+	export interface PWAInfo {
+		pwaInDevEnvironment: boolean;
+		webManifest: {
+			href: string;
+			useCredentials: boolean;
+			linkTag: string;
+		};
+		registerSW?: (options?: RegisterSWOptions) => Promise<RegisterSWResult>;
+	}
+	export const pwaInfo: PWAInfo | undefined;
 }
 
 // Define register svelte module
 declare module 'virtual:pwa-register/svelte' {
-  // Type for registration options
-  export interface RegisterSWOptions {
-    immediate?: boolean;
-    onNeedRefresh?: () => void;
-    onOfflineReady?: () => void;
-    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
-    onRegisteredSW?: (swScriptUrl: string, registration: ServiceWorkerRegistration | undefined) => void;
-    onRegisterError?: (error: Error) => void;
-  }
+	// Type for registration options
+	export interface RegisterSWOptions {
+		immediate?: boolean;
+		onNeedRefresh?: () => void;
+		onOfflineReady?: () => void;
+		onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
+		onRegisteredSW?: (
+			swScriptUrl: string,
+			registration: ServiceWorkerRegistration | undefined
+		) => void;
+		onRegisterError?: (error: Error) => void;
+	}
 
-  // Type for registration result
-  export interface RegisterSWResult {
-    needRefresh: boolean;
-    offlineReady: boolean;
-    updateServiceWorker: (reloadPage?: boolean) => Promise<void>;
-  }
+	// Type for registration result
+	export interface RegisterSWResult {
+		needRefresh: boolean;
+		offlineReady: boolean;
+		updateServiceWorker: (reloadPage?: boolean) => Promise<void>;
+	}
 
-  // Main function exposed by the module
-  export function useRegisterSW(options?: RegisterSWOptions): RegisterSWResult;
+	// Main function exposed by the module
+	export function useRegisterSW(options?: RegisterSWOptions): RegisterSWResult;
 }
+
+// Additional custom type declarations if needed
