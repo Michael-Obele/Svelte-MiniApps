130af8887ddec437d36456ae51538fe1e943baa1
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-08-28 02:43:33 +0100

    💄 style(app.d.ts): add reference for enhanced-img types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+/// <reference types="@sveltejs/enhanced-img" />
+
 declare global {
 	namespace App {
 		interface Locals {
