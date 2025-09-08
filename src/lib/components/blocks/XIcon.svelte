aae0a40185a3220989801fe3fb68864e844af795
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-09-08 22:54:18 +0100

    feat: add Google and X icons with appropriate SVG paths and props handling

---
+++ src/lib/components/blocks/XIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let { class: className = '' }: { class?: string } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	class="size-5 {className}"
+>
+	<path
+		fill="currentColor"
+		d="m17.687 3.063l-4.996 5.711l-4.32-5.711H2.112l7.477 9.776l-7.086 8.099h3.034l5.469-6.25l4.78 6.25h6.102l-7.794-10.304l6.625-7.571zm-1.064 16.06L5.654 4.782h1.803l10.846 14.34z"
+	/>
+</svg>
