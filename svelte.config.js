a3f230195515e7f2ddaeab699cf164b85a8ca4ae
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-09-25 08:22:40 +0100

    refactor: add experimental remoteFunctions option and compilerOptions to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -19,6 +19,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		experimental: {
+			remoteFunctions: true
+		},
 		adapter: adapter(),
 		serviceWorker: {
 			register: false
@@ -27,6 +30,9 @@ const config = {
 			'@/*': './src/lib/components/*'
 		}
 	},
+	compilerOptions: {
+
+},
 
 	extensions: ['.svelte', '.svx']
 };
