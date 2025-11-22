535b648e846e18a1f4ee86ebb503b856d1fad5d8
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-11-22 09:39:23 +0100

    chore: update shadcn svelte component.

--- src/lib/components/ui/separator/separator.svelte
+++ src/lib/components/ui/separator/separator.svelte
@@ -5,18 +5,17 @@
 	let {
 		ref = $bindable(null),
 		class: className,
-		orientation = "horizontal",
+		"data-slot": dataSlot = "separator",
 		...restProps
 	}: SeparatorPrimitive.RootProps = $props();
 </script>
 
 <SeparatorPrimitive.Root
 	bind:ref
+	data-slot={dataSlot}
 	class={cn(
-		"bg-border shrink-0",
-		orientation === "horizontal" ? "h-[1px] w-full" : "min-h-full w-[1px]",
+		"bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
 		className
 	)}
-	{orientation}
 	{...restProps}
 />
